620f514510f038b9e3f1cd0581fe754bcdac801a
Elco Garcia <garciaelco18@gmail.com>
2019-12-20 10:50:17 -0800

    New challenge that builds off previous chall.

--- Algorithm_Practice.js
+++ Algorithm_Practice.js
@@ -44,3 +44,32 @@ function validParentheses(str) {
 		}
 	}
 }
+
+//Practice #3
+// Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+// This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}.
+
+// All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+function validBraces(braces) {
+	let tracer = [];
+	for (let i = 0; i < braces.length; i++) {
+		if (braces[i] === '(' || braces[i] === '{' || braces[i] === '[') {
+			tracer.push(braces[i]);
+		} else {
+			if (tracer.length === 0) return false;
+			let lastValue = tracer[tracer.length - 1];
+			if (
+				(braces[i] === ']' && lastValue === '[') ||
+				(braces[i] === '}' && lastValue === '{') ||
+				(braces[i] === ')' && lastValue === '(')
+			) {
+				tracer.pop();
+			} else {
+				break;
+			}
+		}
+	}
+	return tracer.length === 0;
+}
