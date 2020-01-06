3eb51a1ed0833fa8ebd4055e407d6ecea03bd688
Elco Garcia <garciaelco18@gmail.com>
2020-01-06 11:36:28 -0600

    added moethods  to binary tree

--- Binary_Trees.js
+++ Binary_Trees.js
@@ -83,6 +83,33 @@ class BST {
 		}
 		return false;
 	}
+	isBalanced() {
+		return this.findMinHeight() >= this.findMaxHeight() - 1;
+	}
+	findMinHeight(node = this.root) {
+		if (node == null) {
+			return -1;
+		}
+		let left = this.findMinHeight(node.left);
+		let right = this.findMinHeight(node.right);
+		if (left < right) {
+			return left + 1;
+		} else {
+			return right + 1;
+		}
+	}
+	findMaxHeight(node = this.root) {
+		if (node == null) {
+			return -1;
+		}
+		let left = this.findMaxHeight(node.left);
+		let right = this.findMaxHeight(node.right);
+		if (left > right) {
+			return left + 1;
+		} else {
+			return right + 1;
+		}
+	}
 	remove(data) {
 		const removeNode = function(node, data) {
 			if (node == null) {
