bf034c807885cdb8ae6c04f015399c57acfbc4cb
Elco Garcia <garciaelco18@gmail.com>
2020-01-07 14:00:19 -0600

    spelling error

--- Stacks.js
+++ Stacks.js
@@ -8,7 +8,7 @@ for (var i = 0; i < word.length; i++) {
 	letters.push(word[i]);
 }
 
-//pop off the staack in reverse order
+//pop off the stack in reverse order
 for (var i = 0; i < word.length; i++) {
 	rword += letters.pop();
 }
