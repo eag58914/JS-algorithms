9f534a5de1d61d8dc8aa17ec9e5013c89689a784
Elco Garcia <garciaelco18@gmail.com>
2020-01-15 13:50:54 -0600

    pracitce with Trie

---
+++ Trie.js
@@ -0,0 +1,55 @@
+// a data structure that is used to store  data in steps, often to store words, pssible use case is to check if a word is in a dictionary
+
+let Node = function() {
+	this.keys = new Map();
+	this.end = false;
+	this.setEnd = function() {
+		this.end = true;
+	};
+	this.isEnd = function() {
+		return this.end;
+	};
+
+	let Trie = function() {
+		this.root = new Node();
+		this.add = function(input, node = this.root) {
+			if (input.length === 0) {
+				node.setEnd();
+				return;
+			} else if (!node.keys.has(input[0])) {
+				node.keys.set(input[0], new Node());
+				return this.add(input.substr(1), node.keys.get(input[0]));
+			} else {
+				return this.add(input.substr(1), node.keys.get(input[0]));
+			}
+		};
+	};
+	this.isWord = function(word) {
+		let node = this.root;
+		while (word.length > 1) {
+			if (!node.keys.has(word[0])) {
+				return false;
+			} else {
+				node = node.keys.get(word[0]);
+				word = word.substr(1);
+			}
+		}
+		return node.keys.has(word) && node.keys.get(word).isEnd() ? true : false;
+	};
+	this.print = function() {
+		let words = new Array();
+		let search = function(node, string) {
+			if (node.keys.size != 0) {
+				search(node.keys.get(letter), string.concat(letter));
+			}
+			if (node.isEnd()) {
+				words.push(string);
+			} else {
+				string.length > 0 ? words.push(string) : undefined;
+				return;
+			}
+		};
+		search(this.root, new String());
+		return words.length > 0 ? words : null;
+	};
+};
